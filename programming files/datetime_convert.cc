fd468627dda338e81547a5d42cddd0c5940915f4
Penofcilin <63890812+penofcilin@users.noreply.github.com>
2025-04-01 16:55:51 -0400

    Update datetime_convert.cc

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,16 +5,23 @@
 
 int main()
 {
+    //Creating a date string
     std::string date_str = "2022-03-17 10:45:30";
+    //creating a date object
     std::tm date_obj = {};
-    std::ostringstream ss(date_str);
-    ss >> get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    //creating a stringstream with the date string
+    std::istringstream ss(date_str);
+    //outputting the time to the stringstream
+    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    //creating a separate stream for formatted
     std::stringstream formatted_date_ss;
+    //formatting the date
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
-    std::stringbuffer formatted_date = formatted_date_ss.int();
+    //converting the formatted date to a string
+    std::string formatted_date = formatted_date_ss.str();
 
+    //outputting the formatted date to the console
     std::cout << formatted_date << std::endl;
 
     return 0;
 }
-
